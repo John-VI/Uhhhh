9cd9f00b53f6f33ca51618a536ff00fd7ea8a738
John-VI <dogjaw2233@protonmail.com>
2021-08-03 13:57:19 -0400

    Shmup stuff, forgot to commit before. Lol.

--- Assets/script/scrollingbg.cs
+++ Assets/script/scrollingbg.cs
@@ -7,14 +7,21 @@ public class scrollingbg : MonoBehaviour
     // Start is called before the first frame update
     public float hspeed;
     public float vspeed;
+    public Color tint = new Color(255, 255, 255, 255);
+    public Camera cam;
     float hoffset;
     float voffset;
     Renderer ren;
 
     void Start() {
         ren = GetComponent<Renderer>();
+        Transform trans = GetComponent<Transform>();
+        trans.position = new Vector3(0, 0, 1);
+        float height = 2.0f * cam.orthographicSize;
+        trans.localScale = new Vector3(height * cam.aspect, height, 1);// does
         hoffset = 0;
         voffset = 0;
+        ren.material.SetColor("_Color", tint);
     }
 
     // Update is called once per frame
@@ -26,7 +33,5 @@ public class scrollingbg : MonoBehaviour
 	if (Mathf.Abs(hoffset) > ren.material.mainTexture.width)
 	    hoffset -= ren.material.mainTexture.width * (hoffset/Mathf.Abs(hoffset));
         ren.material.mainTextureOffset = new Vector2(hoffset, voffset);
-        print(hoffset);
-        print(voffset);
     }
 }
